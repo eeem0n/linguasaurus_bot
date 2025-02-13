72a15186214156324586aa2159d1890a6dd4b7ce
Shahriar Hasan Emon <74528589+eeem0n@users.noreply.github.com>
2025-02-14 01:58:48 +0600

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,13 +1,1 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-# Function to generate semester selection buttons
-def semester_menu(category):
-    keyboard = [
-        [InlineKeyboardButton(f" Semester {i}", callback_data=f"{category}_semester_{i}")] for i in range(1, 9)
-    ]
-    return InlineKeyboardMarkup(keyboard)
-
-# Function to generate a back button
-def back_button():
-    keyboard = [[InlineKeyboardButton("🔙 Back", callback_data="go_back")]]
-    return InlineKeyboardMarkup(keyboard)
